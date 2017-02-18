e936c21437e85f090b21e02741cc904d1e0a5546
ben <ben@DESKTOP-AULC2J9>
2017-02-18 23:12:56 +0000

    initial commit

---
+++ brass/brass/ExtensionsIWebElement.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace brass
+{
+    public static class ExtensionsIWebElement
+    {
+        public static object Execute(this IWebElement element, string script)
+        {
+            var driver = ((RemoteWebElement)element).WrappedDriver;
+            var executor = ((IJavaScriptExecutor)driver);
+
+            var wrapper = new StringBuilder();
+            wrapper.Append("(function(){");
+            wrapper.Append(script);
+            wrapper.Append("}).bind(arguments[0])(Array.splice(arguments, 1))");
+
+            var wrappedString = wrapper.ToString();
+            return executor.ExecuteScript(wrappedString);
+        }
+
+        public static AggregateException Try(this object anything, params Action[] actions)
+        {
+            var exceptions = new List<Exception>();
+            foreach (var action in actions)
+            {
+                try
+                {
+                    action();
+                    break;
+                }
+                catch (Exception x)
+                {
+                    exceptions.Add(x);
+                }
+            }
+            if (exceptions.Any())
+            {
+                return new AggregateException(exceptions);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ brass/brass/WebElement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace brass
+{
+    public class WebElement
+    {
+        private IWebElement reference;
+        private WebElement parent;
+
+        private WebElement(IWebElement elementRoot)
+        {
+            this.reference = elementRoot;
+        }
+
+        private WebElement(IWebElement elementRoot, WebElement parent)
+            : this(elementRoot)
+        {
+            this.parent = parent;
+        }
+        public static WebElement Root(IWebElement rootElement)
+        {
+            return new WebElement(rootElement);
+        }
+
+        public WebElement Find(string query)
+        {
+            IWebElement element = null;
+            var agg = this.Try(
+                () => element = this.reference.FindElement(By.CssSelector(query)),
+                () => element = (IWebElement)this.reference.Execute($"return this.$({query})")
+            );
+            if (element == null)
+            {
+                throw agg;
+            }
+            return new WebElement(element, this);
+
+        }
+
+        public WebElement Click()
+        {
+            this.Try(
+                () => this.reference.Click(),
+                () => this.reference.Execute("this.click();")
+            );
+            return this;
+        }
+    }
+}
